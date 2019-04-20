8e923f25a6a34e1af71bcaeea8b36053e3cef955
DevriesL <therkduan@gmail.com>
2019-04-20 22:33:12 +0800

    zstd: merge optimization from Samsung
    Pzqqt: Original commit: https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1/commit/d3022d1ea973ab3a0b5e30a1802c2945a21eeea3,
    This is a partial merge, and port to the zstd-1.4.10.
    
    Change-Id: I7f88b1555bededaf7741bfd5edf60a860a2ecb53
    Signed-off-by: DevriesL <therkduan@gmail.com>
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: kanonifyX <kanonify01@gmail.com>

--- crypto/zstd.c
+++ crypto/zstd.c
@@ -22,9 +22,6 @@
 #include <linux/zstd.h>
 #include <crypto/internal/scompress.h>
 
-
-#define ZSTD_DEF_LEVEL	1
-
 struct zstd_ctx {
 	zstd_cctx *cctx;
 	zstd_dctx *dctx;
@@ -34,7 +31,11 @@ struct zstd_ctx {
 
 static zstd_parameters zstd_params(void)
 {
-	return zstd_get_params(ZSTD_DEF_LEVEL, 0);
+	if (compression_level == 0)
+		compression_level = 1;
+	if (compression_level > zstd_max_clevel())
+		compression_level = zstd_max_clevel();
+	return zstd_get_params(compression_level, PAGE_SIZE);
 }
 
 static int zstd_comp_init(struct zstd_ctx *ctx)
