ba400d09377640b645b5d76159494f38874cd380
Gao Xiang <hsiangkao@redhat.com>
2021-03-29 09:23:07 +0800

    erofs: introduce on-disk lz4 fs configurations
    Introduce z_erofs_lz4_cfgs to store all lz4 configurations.
    Currently it's only max_distance, but will be used for new
    features later.
    
    Link: https://lore.kernel.org/r/20210329012308.28743-4-hsiangkao@aol.com
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <hsiangkao@redhat.com>
    Change-Id: Ic6db91a5075cfb788b721ffef41c27d144a69602

--- fs/erofs/erofs_fs.h
+++ fs/erofs/erofs_fs.h
@@ -219,6 +219,12 @@ struct z_erofs_lz4_cfgs {
 	u8 reserved[10];
 } __packed;
 
+/* 14 bytes (+ length field = 16 bytes) */
+struct z_erofs_lz4_cfgs {
+	__le16 max_distance;
+	u8 reserved[12];
+} __packed;
+
 /*
  * bit 0 : COMPACTED_2B indexes (0 - off; 1 - on)
  *  e.g. for 4k logical cluster size,      4B        if compacted 2B is off;
