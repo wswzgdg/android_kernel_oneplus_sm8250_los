6fe6204ebc365ce9e8da90d0bf5ab3088b58dbad
John Galt <johngaltfirstrun@gmail.com>
2022-03-15 11:39:40 -0400

    treewide: move some perf/prime affines to hp affine
    It was found poor irq balancing behavior from these changes can actually
    increase latency. Allow sane balancing for these kthreads/irq.
    
    DRM was kept on prime because balancing has negative impacts.
    pp_event was moved to hp because it's quite heavy and not inherently
    latency sensitive.

--- fs/exec.c
+++ fs/exec.c
@@ -85,6 +85,12 @@ int suid_dumpable = 0;
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
+#define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
+#define QTIHW_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware"
+#define UDFPS_BIN_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint"
+#define SFLINGER_BIN_PREFIX "/system/bin/surfaceflinger"
+#define NETD_BIN_PREFIX "/system/bin/netd"
+
 void __register_binfmt(struct linux_binfmt * fmt, int insert)
 {
 	BUG_ON(!fmt);
@@ -1883,6 +1889,35 @@ static int __do_execve_file(int fd, struct filename *filename,
 	retval = exec_binprm(bprm);
 	if (retval < 0)
 		goto out;
+
+	if (capable(CAP_SYS_ADMIN)) {
+                if (unlikely(!strncmp(filename->name,
+					   HWCOMPOSER_BIN_PREFIX,
+					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->pc_flags |= PC_HP_AFFINE;
+			set_cpus_allowed_ptr(current, cpu_hp_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   UDFPS_BIN_PREFIX,
+					   strlen(UDFPS_BIN_PREFIX)))) {
+			current->pc_flags |= PC_HP_AFFINE;
+			set_cpus_allowed_ptr(current, cpu_hp_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   SFLINGER_BIN_PREFIX,
+					   strlen(SFLINGER_BIN_PREFIX)))) {
+			current->pc_flags |= PC_HP_AFFINE;
+			set_cpus_allowed_ptr(current, cpu_hp_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   NETD_BIN_PREFIX,
+					   strlen(NETD_BIN_PREFIX)))) {
+			current->pc_flags |= PC_HP_AFFINE;
+			set_cpus_allowed_ptr(current, cpu_hp_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   QTIHW_BIN_PREFIX,
+					   strlen(QTIHW_BIN_PREFIX)))) {
+			current->pc_flags |= PC_HP_AFFINE;
+			set_cpus_allowed_ptr(current, cpu_hp_mask);
+		}
+	}
 
 	/* execve succeeded */
 	current->fs->in_exec = 0;
