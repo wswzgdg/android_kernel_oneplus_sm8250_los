b8724511882f9407f094b35b06b9267ea3895742
brokestar233 <3765589194@qq.com>
2025-09-13 14:11:29 +0800

    lz4: Use ARM64 NEON-optimized decompression where available

--- crypto/lz4hc.c
+++ crypto/lz4hc.c
@@ -95,7 +95,11 @@ static int lz4hc_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
 static int __lz4hc_decompress_crypto(const u8 *src, unsigned int slen,
 				     u8 *dst, unsigned int *dlen, void *ctx)
 {
+#if defined(CONFIG_ARM64) && defined(CONFIG_KERNEL_MODE_NEON)
+	int out_len = LZ4_arm64_decompress_safe(src, dst, slen, *dlen, false);
+#else
 	int out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
+#endif
 
 	if (out_len < 0)
 		return -EINVAL;
