aacaa5d5526698be5a9efabb94fbd6564025ac8b
kakarot-super <kakarot@example.com>
2026-02-24 23:30:12 +0800

    erofs: Use ARM64 v8 ASM to accelerate lz4 decompression Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- fs/erofs/decompressor.c
+++ fs/erofs/decompressor.c
@@ -232,16 +232,16 @@ if (rq->partial_decoding || !support_0padding) {
         ret = LZ4_arm64_decompress_safe_partial(src + inputmargin, out,
                       rq->inputsize, rq->outputsize, rq->inplace_io);
 #else
-        ret = LZ4_decompress_safe_partial(src + inputmargin, out,
-                rq->inputsize, rq->outputsize, rq->outputsize);
+		ret = LZ4_arm64_decompress_safe_partial(src + inputmargin, out,
+				rq->inputsize, rq->outputsize, rq->inplace_io);
 #endif
     } else {
 #if defined(CONFIG_ARM64) && defined(CONFIG_KERNEL_MODE_NEON)
         ret = LZ4_arm64_decompress_safe(src + inputmargin, out,
                       rq->inputsize, rq->outputsize, rq->inplace_io);
 #else
-        ret = LZ4_decompress_safe(src + inputmargin, out,
-                      rq->inputsize, rq->outputsize);
+		ret = LZ4_arm64_decompress_safe(src + inputmargin, out,
+					  rq->inputsize, rq->outputsize, rq->inplace_io);
 #endif
     }
 bypass_decompression:
