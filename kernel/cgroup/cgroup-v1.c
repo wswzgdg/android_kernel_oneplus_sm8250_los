86e767b786bbf05cda33a475c353fa4878ab0da9
kakarot-super <kakarot@example.com>
2026-03-06 18:10:55 +0800

    cgroup: Remove unnecessary include of cpu_input_boost.h

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -15,7 +15,7 @@
 #include <linux/cgroupstats.h>
 #include <linux/cpu.h>
 #include <linux/binfmts.h>
-#include <linux/cpu_input_boost.h>
+// #include <linux/cpu_input_boost.h>
 
 #include <trace/events/cgroup.h>
 
